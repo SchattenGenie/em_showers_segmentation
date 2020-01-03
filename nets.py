59d01a8fe8613d70764d6879e848f493ff662781
vbelavin <belavin@phystech.edu>
2020-01-03 16:36:28 +0300

    refactoring model

--- nets.py
+++ nets.py
@@ -10,6 +10,7 @@ import torch_geometric.transforms as T
 import torch_cluster
 from torch_geometric.nn import NNConv, GCNConv, GraphConv
 from torch_geometric.nn import PointConv, EdgeConv, SplineConv
+from hgcn.layers.layers import FermiDiracDecoder
 
 
 class EmulsionConv(MessagePassing):
@@ -32,17 +33,17 @@ class EmulsionConv(MessagePassing):
 
 
 class GraphNN_KNN_v1(nn.Module):
-    def __init__(self, k, dim_out=10):
+    def __init__(self, input_dim, output_dim=10):
         super().__init__()
-        self.k = k
-        self.emconv = EmulsionConv(self.k, self.k)
+        self.input_dim = input_dim
+        self.emconv = EmulsionConv(self.input_dim, self.input_dim)
         self.wconv1 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
         self.wconv2 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
         self.wconv3 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.output = nn.Linear(10, dim_out)
+        self.output = nn.Linear(10, output_dim)
 
     def forward(self, data):
-        x, edge_index, orders = data.x, data.edge_index, data.mask
+        x, edge_index, orders = data.x, data.edge_index, data.orders
         x = self.emconv(x=x, edge_index=edge_index, orders=orders)
         x1 = self.wconv1(x=x, edge_index=edge_index)
         x2 = self.wconv2(x=x1, edge_index=edge_index)
@@ -51,10 +52,10 @@ class GraphNN_KNN_v1(nn.Module):
 
 
 class EdgeClassifier_v1(nn.Module):
-    def __init__(self, dim_out):
+    def __init__(self, output_dim, **kwargs):
         super().__init__()
         self._layers = nn.ModuleList([
-            nn.Linear(dim_out * 2, 144),
+            nn.Linear(output_dim * 2, 144),
             nn.Tanh(),
             nn.Linear(144, 144),
             nn.Tanh(),
@@ -64,7 +65,56 @@ class EdgeClassifier_v1(nn.Module):
             nn.Sigmoid()
         ])
 
-    def forward(self, x):
+    def forward(self, shower, embeddings, edge_index):
+        embeddings = torch.cat([
+            embeddings[edge_index[0]],
+            embeddings[edge_index[1]]
+        ], dim=1)
         for layer in self._layers:
-            x = layer(x)
-        return x
+            embeddings = layer(embeddings)
+        return embeddings
+
+
+class DiracClassifier(nn.Module):
+    def __init__(self, manifold, c=1., r=0., t=1., **kwargs):
+        super().__init__()
+        self.manifold = manifold
+        self.c = c
+        self.dc = FermiDiracDecoder(r=r, t=t)
+
+    def forward(self, shower, embeddings, edge_index):
+        emb_in = embeddings[edge_index[0], :]
+        emb_out = embeddings[edge_index[1], :]
+        sqdist = self.manifold.sqdist(emb_in, emb_out, self.c)
+        probs = self.dc.forward(sqdist)
+        return probs
+
+
+class EdgeDenseClassifier(nn.Module):
+    def __init__(self, input_dim=10, **kwargs):
+        super(EdgeDenseCl, self).__init__()
+        self.edge_classifier = nn.Sequential(
+            nn.Linear(input_dim, 8),
+            nn.Tanh(),
+            nn.Linear(8, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, embeddings, edge_index):
+        return self.edge_classifier(torch.cat([embeddings[edge_index[0]], embeddings[edge_index[0]]], 1))
+
+
+class EdgeDenseClassifierEdgeAttribute(nn.Module):
+    def __init__(self, input_dim=10, **kwargs):
+        super(EdgeDenseCl, self).__init__()
+        self.edge_classifier = nn.Sequential(
+            nn.Linear(input_dim, 8),
+            nn.Tanh(),
+            nn.Linear(8, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, shower, embeddings, edge_index):
+        x = torch.cat([embeddings[edge_index[0]], embeddings[edge_index[0]]], 1)
+        x = torch.cat([x, shower.edge_attr])
+        return self.edge_classifier(x)
