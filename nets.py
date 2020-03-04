294d27fc266dad6153ac725df534a32e20ce47cf
vbelavin <belavin@phystech.edu>
2020-03-04 15:31:21 +0300

    speeding up training, different architecture

--- nets.py
+++ nets.py
@@ -5,15 +5,99 @@ import torch.optim as optim
 from torch.nn import Sequential
 import torch_geometric
 from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import add_self_loops, degree
+from torch_geometric.utils import add_self_loops, degree, scatter_
 import torch_geometric.transforms as T
 import torch_cluster
 from torch_geometric.nn import NNConv, GCNConv, GraphConv
 from torch_geometric.nn import PointConv, EdgeConv, SplineConv
-from hgcn.layers.layers import FermiDiracDecoder
+from torch.utils.checkpoint import checkpoint
+import numpy as np
+
+
+def extract_subgraph(h, adj, edge_attr, order):
+    adj_selected = adj[:, order]
+    edge_attr_selected = edge_attr[order, :]
+    nodes_selected = adj_selected.unique()
+    h_selected = h[nodes_selected]
+    nodes_selected_new = torch.arange(len(nodes_selected))
+    dictionary = dict(zip(nodes_selected.cpu().numpy(), nodes_selected_new.cpu().numpy()))
+    adj_selected_new = torch.tensor(np.vectorize(dictionary.get)(adj_selected.cpu().numpy())).long().to(adj)
+    return h_selected, nodes_selected, edge_attr_selected, adj_selected_new
 
 
 class EmulsionConv(MessagePassing):
+    def __init__(self, in_channels, out_channels, edge_dim=1, direction=0):
+        super().__init__(aggr='add')
+        self.mp = nn.Sequential(
+            nn.Linear(in_channels * 2 + edge_dim, out_channels),
+            nn.ReLU()
+        )
+        self._direction = direction  # TODO: defines direction
+
+    def forward(self, x, edge_index, orders, edge_attr, orders_preprocessed):
+        x = x.clone()
+        for i, order in enumerate(orders):
+            if order.sum():
+                # print(i, len(orders_preprocessed), orders_preprocessed[i])
+                nodes_selected, adj_selected_new = orders_preprocessed[i]
+                nodes_selected = nodes_selected.to(x).long()
+                adj_selected_new = adj_selected_new.to(x).long()
+                x_selected = x[nodes_selected]
+                edge_attr_selected = edge_attr[order, :]
+                x_selected = self.message(
+                    x_j=x_selected[adj_selected_new[0]],
+                    x_i=x_selected[adj_selected_new[1]],
+                    edge_attr=edge_attr_selected
+                )
+                x_selected = scatter_('add', x_selected, adj_selected_new[self._direction],
+                                      dim=0, dim_size=len(nodes_selected))
+                x[nodes_selected] = (x[nodes_selected] + x_selected) / 2.
+        return x
+
+    def message(self, x_j, x_i, edge_attr):
+        return self.mp(torch.cat([x_i, x_j - x_i, edge_attr.view(len(x_i), -1)], dim=1))
+
+    def update(self, aggr_out, x):
+        return aggr_out + x
+
+
+class EmulsionConvSlow(MessagePassing):
+    def __init__(self, in_channels, out_channels, edge_dim=1, direction=0):
+        super().__init__(aggr='add')
+        self.mp = nn.Sequential(
+            nn.Linear(in_channels * 2 + edge_dim, out_channels),
+            nn.ReLU()
+        )
+        self._direction = direction  # TODO: defines direction
+
+    def forward(self, x, edge_index, orders, edge_attr):
+        x = x.clone()
+        for order in orders:
+            if order.sum():
+                x_selected, nodes_selected, edge_attr_selected, adj_selected_new = extract_subgraph(
+                    h=x,
+                    adj=edge_index,
+                    edge_attr=edge_attr,
+                    order=order
+                )
+                x_selected = self.message(
+                    x_j=x_selected[adj_selected_new[0]],
+                    x_i=x_selected[adj_selected_new[1]],
+                    edge_attr=edge_attr_selected
+                )
+                x_selected = scatter_('add', x_selected, adj_selected_new[self._direction],
+                                      dim=0, dim_size=len(nodes_selected))
+                x[nodes_selected] = (x[nodes_selected] + x_selected) / 2.
+        return x
+
+    def message(self, x_j, x_i, edge_attr):
+        return self.mp(torch.cat([x_i, x_j - x_i, edge_attr.view(len(x_i), -1)], dim=1))
+
+    def update(self, aggr_out, x):
+        return aggr_out + x
+
+
+class EmulsionConvOldSlow(MessagePassing):
     def __init__(self, in_channels, out_channels):
         super().__init__(aggr='add')
         self.mp = torch.nn.Linear(in_channels * 2, out_channels)
@@ -32,62 +116,78 @@ class EmulsionConv(MessagePassing):
         return aggr_out + x
 
 
+def init_bias_model(model, b: float):
+    for module in model.modules():
+        if hasattr(module, 'bias'):
+            module.bias.data.fill_(b)
+
+
 class GraphNN_KNN_v1(nn.Module):
-    def __init__(self, input_dim, output_dim=10):
+    def __init__(self, input_dim, hidden_dim, output_dim=10, bias_init=0., **kwargs):
         super().__init__()
         self.input_dim = input_dim
-        self.emconv = EmulsionConv(self.input_dim, self.input_dim)
-        self.wconv1 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.wconv2 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.wconv3 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.output = nn.Linear(10, output_dim)
+        self.hidden_dim = hidden_dim
+        self.linear1 = nn.Sequential(nn.Linear(self.input_dim, self.hidden_dim), nn.ReLU())
+        self.emconv1 = EmulsionConv(self.hidden_dim, self.hidden_dim)
+        self.linear2 = nn.Sequential(nn.Linear(self.hidden_dim, self.hidden_dim), nn.ReLU())
+        self.emconv2 = EmulsionConv(self.hidden_dim, self.hidden_dim)
+        self.wconv1 = EdgeConv(Sequential(nn.Linear(2 * self.hidden_dim, self.hidden_dim), nn.ReLU()), 'max')
+        self.wconv2 = EdgeConv(Sequential(nn.Linear(2 * self.hidden_dim, self.hidden_dim), nn.ReLU()), 'max')
+        self.wconv3 = EdgeConv(Sequential(nn.Linear(2 * self.hidden_dim, self.hidden_dim), nn.ReLU()), 'max')
+        self.wconv4 = EdgeConv(Sequential(nn.Linear(2 * self.hidden_dim, self.hidden_dim), nn.ReLU()), 'max')
+        self.output = nn.Linear(self.hidden_dim, output_dim)
+        init_bias_model(self, b=0.)
 
     def forward(self, data):
-        x, edge_index, orders = data.x, data.edge_index, data.orders
-        x = self.emconv(x=x, edge_index=edge_index, orders=orders)
-        x1 = self.wconv1(x=x, edge_index=edge_index)
-        x2 = self.wconv2(x=x1, edge_index=edge_index)
-        x3 = self.wconv3(x=x2, edge_index=edge_index)
-        return self.output(x3)
+        x, edge_index, orders, edge_attr = data.x, data.edge_index, data.orders, data.edge_attr
+        orders_preprocessed = data.orders_preprocessed[0]
+        print("orders preprocessed", len(orders_preprocessed), len(orders))
+
+        x = self.linear1(x)
+        x = self.emconv1(x=x, edge_index=edge_index, orders=orders, edge_attr=edge_attr, orders_preprocessed=orders_preprocessed)
+        # x = x + x_new
+        x = self.linear2(x)
+        x = self.emconv2(x=x, edge_index=edge_index, orders=orders, edge_attr=edge_attr, orders_preprocessed=orders_preprocessed)
+        # x = x + x_new
+        # x = checkpoint(self.emconv, x=x, edge_index=edge_index, orders=orders)
+        x = self.wconv1(x=x, edge_index=edge_index)
+        # x = x + x_new
+        # x = checkpoint(self.emconv, x=x, edge_index=edge_index, orders=orders)
+        x = self.wconv2(x=x, edge_index=edge_index)
+        # x = x + x_new
+        # x = checkpoint(self.emconv, x=x, edge_index=edge_index, orders=orders)
+        x = self.wconv3(x=x, edge_index=edge_index)
+        # x = x + x_new
+        x = self.wconv4(x=x, edge_index=edge_index)
+        return self.output(x)
 
 
 class EdgeClassifier_v1(nn.Module):
-    def __init__(self, output_dim, **kwargs):
+    def __init__(self, input_dim, hidden_dim=32, prior_proba=1. - 0.025, **kwargs):
         super().__init__()
         self._layers = nn.ModuleList([
-            nn.Linear(output_dim * 2, 144),
-            nn.Tanh(),
-            nn.Linear(144, 144),
-            nn.Tanh(),
-            nn.Linear(144, 32),
-            nn.Tanh(),
-            nn.Linear(32, 1),
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 1),
             nn.Sigmoid()
         ])
+        init_bias_model(self, b=0.)
+        init_bias_model(self._layers[-2], b=-np.log((1 - prior_proba) / prior_proba))
 
     def forward(self, shower, embeddings, edge_index):
         embeddings = torch.cat([
             embeddings[edge_index[0]],
-            embeddings[edge_index[1]]
+            embeddings[edge_index[1]],
+            shower.edge_attr
         ], dim=1)
         for layer in self._layers:
             embeddings = layer(embeddings)
+            # x = checkpoint(layer, embeddings)
         return embeddings
-
-
-class DiracClassifier(nn.Module):
-    def __init__(self, manifold, c=1., r=0., t=1., **kwargs):
-        super().__init__()
-        self.manifold = manifold
-        self.c = c
-        self.dc = FermiDiracDecoder(r=r, t=t)
-
-    def forward(self, shower, embeddings, edge_index):
-        emb_in = embeddings[edge_index[0], :]
-        emb_out = embeddings[edge_index[1], :]
-        sqdist = self.manifold.sqdist(emb_in, emb_out, self.c)
-        probs = self.dc.forward(sqdist)
-        return 2. * probs
 
 
 class EdgeDenseClassifier(nn.Module):
--- training_classifier.py
+++ training_classifier.py
@@ -1,32 +1,24 @@
 import comet_ml
-from comet_ml import Experiment
+from comet_ml import OfflineExperiment as Experiment
 import click
 import numpy as np
 import torch
 import torch.nn as nn
-from nets import *
-from hgcn.models.hgcn_nets import HGCN, HGCNResidual, HGCNResidualEmulsionConv
-from hgcn.optimizers import RiemannianAdam
+from torch.utils.data._utils import pin_memory
+from nets import GraphNN_KNN_v1, EdgeClassifier_v1
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, accuracy_score, average_precision_score
 from torch_geometric.data import DataLoader
 from preprocessing import preprocess_dataset
 from viz_utils import RunningAverageMeter, plot_aucs
-from hgcn import manifolds
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import classification_losses
+from sys_utils import get_freer_gpu
+from roc_auc.roc_auc import fast_auc
+from pathlib import Path
 import sys
 import os
-
-
-def get_freer_gpu():
-    """
-    Function to get the freest GPU available in the system
-    :return:
-    """
-    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
-    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
-    return np.argmax(memory_available)
 
 
 def str_to_class(classname: str):
@@ -44,98 +36,76 @@ def predict_one_shower(shower, graph_embedder, edge_classifier):
     embeddings = graph_embedder(shower)
     edge_labels_true = (shower.y[shower.edge_index[0]] == shower.y[shower.edge_index[1]]).view(-1)
     edge_labels_predicted = edge_classifier(shower=shower, embeddings=embeddings, edge_index=shower.edge_index).view(-1)
-    return edge_labels_true, torch.clamp(2 * edge_labels_predicted, 1e-6, 1 - 1e-6)
+    return edge_labels_true, torch.clamp(edge_labels_predicted, 1e-6, 1 - 1e-6)
 
 
 @click.command()
 @click.option('--datafile', type=str, default='./data/train.pt')
 @click.option('--project_name', type=str, prompt='Enter project name', default='em_showers_network_training')
 @click.option('--work_space', type=str, prompt='Enter workspace name')
-@click.option('--epochs', type=int, default=1000)
+@click.option('--epochs', type=int, default=4)
 @click.option('--learning_rate', type=float, default=1e-3)
-@click.option('--gcn_dim', type=int, default=12)
 @click.option('--hidden_dim', type=int, default=12)
+@click.option('--output_dim', type=int, default=12)
 @click.option('--num_layers', type=int, default=3)
-@click.option('--hyperbolic', type=bool, default=False)
 @click.option('--graph_embedder', type=str, default='GraphNN_KNN_v1')
 @click.option('--edge_classifier', type=str, default='EdgeClassifier_v1')
 def main(
         datafile='./data/train_.pt',
         epochs=1000,
         learning_rate=1e-3,
-        gcn_dim=12,
-        hidden_dim=12,  # obsolete for now ;()
+        hidden_dim=12,
+        output_dim=12,
         num_layers=3,
         project_name='em_showers_net_training',
         work_space='schattengenie',
-        hyperbolic=False,
         graph_embedder='GraphNN_KNN_v1',
         edge_classifier='EdgeClassifier_v1'
 ):
     if torch.cuda.is_available():
-        device = torch.device('cuda:{}'.format(get_freer_gpu()))
+        device = torch.device('cuda:0')  # torch.device('cuda:{}'.format(get_freer_gpu()))
     else:
         device = torch.device('cpu')
     print("Using device = {}".format(device))
 
-    edge_dim = 1
-    experiment = Experiment(project_name=project_name, workspace=work_space)
+    experiment = Experiment(project_name=project_name, workspace=work_space, offline_directory="/home/vbelavin/comet_ml_offline")
     device = torch.device(device)
     showers = preprocess_dataset(datafile)
+    # showers = [pin_memory(shower) for shower in showers]
     showers_train, showers_test = train_test_split(showers, random_state=1337)
 
     train_loader = DataLoader(showers_train, batch_size=1, shuffle=True)
     test_loader = DataLoader(showers_test, batch_size=1, shuffle=True)
 
     input_dim = showers[0].x.shape[1]
+    edge_dim = 1
+    graph_embedder = GraphNN_KNN_v1(  # str_to_class(graph_embedder)
+        output_dim=output_dim,
+        hidden_dim=hidden_dim,
+        input_dim=input_dim,
+        num_layers=num_layers
+    ).to(device)
+    edge_classifier = EdgeClassifier_v1( # str_to_class(edge_classifier)
+        input_dim=2 * output_dim + edge_dim,
+    ).to(device)
+    optimizer = torch.optim.Adam(
+        list(graph_embedder.parameters()) + list(edge_classifier.parameters()),
+        lr=learning_rate
+    )
+    class_prior = 0.025
+    criterion = classification_losses.FocalLoss(gamma=3., alpha=1 - class_prior, device=device).to(device)
 
-    if hyperbolic:
-        manifold = manifolds.PoincareBall()
-        graph_embedder = str_to_class(graph_embedder)(
-            manifold=manifold,
-            output_dim=gcn_dim,
-            hidden_dim=gcn_dim,
-            input_dim=input_dim,
-            edge_dim=edge_dim,
-            num_layers=num_layers
-        ).to(device)
-
-        edge_classifier = str_to_class(edge_classifier)(
-            manifold=manifold,
-            input_dim=2 * gcn_dim + edge_dim,
-            c=graph_embedder.curvatures[-1]
-        ).to(device)
-        optimizer = RiemannianAdam(
-            list(graph_embedder.parameters()) + list(edge_classifier.parameters()),
-            lr=learning_rate, stabilize=True
-        )
-    else:
-        graph_embedder = str_to_class(graph_embedder)(
-            output_dim=gcn_dim,
-            hidden_dim=gcn_dim,
-            input_dim=input_dim,
-            num_layers=num_layers
-        ).to(device)
-        edge_classifier = str_to_class(edge_classifier)(
-            input_dim=2 * gcn_dim + edge_dim
-        ).to(device)
-        optimizer = torch.optim.Adam(
-            list(graph_embedder.parameters()) + list(edge_classifier.parameters()),
-            lr=learning_rate  # , stabilize=True
-        )
-    # criterion = nn.BCELoss()
-    criterion = classification_losses.FocalLoss(gamma=2.)
-    # torch.optim.Adam
     loss_train = RunningAverageMeter()
     loss_test = RunningAverageMeter()
     roc_auc_test = RunningAverageMeter()
     pr_auc_test = RunningAverageMeter()
     acc_test = RunningAverageMeter()
     class_disbalance = RunningAverageMeter()
-
-    for _ in tqdm(range(epochs)):
+    best_roc_auc = 0.
+    for epoch in tqdm(range(epochs)):
         for shower in train_loader:
             shower = shower.to(device)
+            print(len(shower))
             edge_labels_true, edge_labels_predicted = predict_one_shower(shower,
                                                                          graph_embedder=graph_embedder,
                                                                          edge_classifier=edge_classifier)
@@ -147,11 +117,12 @@ def main(
             optimizer.step()
             loss_train.update(loss.item())
             class_disbalance.update((edge_labels_true.sum().float() / len(edge_labels_true)).item())
-
+        torch.cuda.empty_cache()
         y_true_list = []
         y_pred_list = []
         for shower in test_loader:
             shower = shower.to(device)
+            print(len(shower))
             edge_labels_true, edge_labels_predicted = predict_one_shower(shower,
                                                                          graph_embedder=graph_embedder,
                                                                          edge_classifier=edge_classifier)
@@ -162,31 +133,33 @@ def main(
             y_true_list.append(y_true)
             y_pred_list.append(y_pred)
             acc = accuracy_score(y_true, y_pred.round())
-            roc_auc = roc_auc_score(y_true, y_pred)
-            pr_auc = average_precision_score(y_true, y_pred)
+            roc_auc = fast_auc(y_true.astype(np.float64), y_pred.astype(np.float64)) - fast_auc(y_true.astype(np.float64), -shower.edge_attr.view(-1).detach().cpu().numpy().astype(np.float64))
+            # pr_auc = average_precision_score(y_true, y_pred) - average_precision_score(y_true, -shower.edge_attr.view(-1).detach().cpu().numpy())
+            # print(roc_auc)
             loss_test.update(loss.item())
             acc_test.update(acc)
             roc_auc_test.update(roc_auc)
-            pr_auc_test.update(pr_auc)
-            print(loss.item(), roc_auc, pr_auc)
+            # pr_auc_test.update(pr_auc)
+            print(loss.item(), roc_auc)
             class_disbalance.update((edge_labels_true.sum().float() / len(edge_labels_true)).item())
-
+        torch.cuda.empty_cache()
 
         experiment.log_metric('loss_test', loss_test.val)
         experiment.log_metric('acc_test', acc_test.val)
         experiment.log_metric('roc_auc_test', roc_auc_test.val)
-        experiment.log_metric('pr_auc_test', pr_auc_test.val)
+        # experiment.log_metric('pr_auc_test', pr_auc_test.val)
         experiment.log_metric('class_disbalance', class_disbalance.val)
 
         y_true = np.concatenate(y_true_list)
         y_pred = np.concatenate(y_pred_list)
-        f = plot_aucs(y_true=y_true, y_pred=y_pred)
-        experiment.log_figure("Optimization dynamic", f, overwrite=True)
+        # f = plot_aucs(y_true=y_true, y_pred=y_pred)
+        # experiment.log_figure("Optimization dynamic", f, overwrite=True)
+        # plt.close(f)
         experiment_key = experiment.get_key()
-        # TODO: save best
-        torch.save(graph_embedder.state_dict(), "graph_embedder_{}.pt".format(experiment_key))
-        torch.save(edge_classifier.state_dict(), "edge_classifier_{}.pt".format(experiment_key))
-
+        if roc_auc_test.val > best_roc_auc:
+            best_roc_auc = roc_auc_test.val
+            torch.save(graph_embedder.state_dict(), "graph_embedder_{}_{}.pt".format(Path(datafile).stem, experiment_key))
+            torch.save(edge_classifier.state_dict(), "edge_classifier_{}_{}.pt".format(Path(datafile).stem, experiment_key))
 
 if __name__ == "__main__":
     main()
