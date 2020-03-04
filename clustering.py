fixing bugs in clustering, adding baseline computation

clustering.py
@@ -1,10 +1,10 @@
 import comet_ml
-from comet_ml import Experiment
+from comet_ml import OfflineExperiment as Experiment
 import numpy as np
 import torch
 import torch.nn as nn
 import click
-from nets import GraphNN_KNN
+from nets import *
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, accuracy_score, average_precision_score
 from torch_geometric.data import DataLoader
@@ -17,22 +17,64 @@ import clustering_metrics
 from clustering_metrics import class_disbalance_graphx, class_disbalance_graphx__
 from clustering_metrics import estimate_e, estimate_start_xyz, estimate_txty
 from sklearn.linear_model import TheilSenRegressor, LinearRegression, HuberRegressor
+from sys_utils import get_freer_gpu, str_to_class
+import itertools
+from operator import itemgetter
+import sys
+
+
+def str_to_class(classname: str):
+    """
+    Function to get class object by its name signature
+    :param classname: str
+        name of the class
+    :return: class object with the same name signature as classname
+    """
+    return getattr(sys.modules[__name__], classname)
 
 
 def predict_one_shower(shower, graph_embedder, edge_classifier):
+    # TODO: batch training
     embeddings = graph_embedder(shower)
-    edge_labels_true = (shower.y[shower.edge_index[0]] == shower.y[shower.edge_index[1]]).view(-1)
-    edge_data = torch.cat([
-        embeddings[shower.edge_index[0]],
-        embeddings[shower.edge_index[1]]
-    ], dim=1)
-    edge_labels_predicted = edge_classifier(edge_data).view(-1)
-
-    return edge_labels_true, edge_labels_predicted
+    edge_labels_true = (~(shower.y[shower.edge_index[0]] == shower.y[shower.edge_index[1]])).view(-1)
+    edge_labels_predicted = edge_classifier(shower=shower, embeddings=embeddings, edge_index=shower.edge_index).view(-1)
+    return edge_labels_true, torch.clamp(edge_labels_predicted, 1e-6, 1 - 1e-6)
 
 
-def preprocess_torch_shower_to_nx(shower, graph_embedder, edge_classifier, threshold=0.5):
-    node_id = 0
+def k_nearest_cut_succ(graphx, k):
+    for node_id in tqdm(graphx.nodes()):
+        successors = list(graphx.successors(node_id))
+        if len(successors) <= k:
+            continue
+        edges = list(itertools.product([node_id], successors))
+        weights = []
+        for edge in edges:
+            weights.append(graphx[edge[0]][edge[1]]['weight'])
+        weights, edges = [list(x) for x in zip(*sorted(zip(weights, edges), key=itemgetter(0)))]
+        edges_to_remove = edges[k:]
+        if len(edges_to_remove):
+            graphx.remove_edges_from(edges_to_remove)
+    return graphx
+
+
+def k_nearest_cut_pred(graphx, k):
+    for node_id in tqdm(graphx.nodes()):
+        predecessors = list(graphx.predecessors(node_id))
+        if len(predecessors) <= k:
+            continue
+        edges = list(itertools.product(predecessors, [node_id]))
+        weights = []
+        for edge in edges:
+            weights.append(graphx[edge[0]][edge[1]]['weight'])
+        weights, edges = [list(x) for x in zip(*sorted(zip(weights, edges), key=itemgetter(0)))]
+
+        edges_to_remove = edges[k:]
+        if len(edges_to_remove):
+            graphx.remove_edges_from(edges_to_remove)
+    return graphx
+
+
+def preprocess_torch_shower_to_nx(shower, graph_embedder, edge_classifier, threshold=0.5, baseline=False):
     G = nx.DiGraph()
     nodes_to_add = []
     showers_data = []
@@ -52,6 +94,7 @@ def preprocess_torch_shower_to_nx(shower, graph_embedder, edge_classifier, thres
                 'ele_TY': shower_data[5]
             }
         )
+    node_id = 0
     for k in range(len(y)):
         nodes_to_add.append(
             (
@@ -71,13 +114,24 @@ def preprocess_torch_shower_to_nx(shower, graph_embedder, edge_classifier, thres
         node_id += 1
 
     edges_to_add = []
-    _, weights = predict_one_shower(shower, graph_embedder=graph_embedder, edge_classifier=edge_classifier)
-    weights = weights.detach().cpu().numpy()
-    edge_index = shower.edge_index.t().detach().cpu().numpy()
-    edge_index = edge_index[weights > threshold]
-    weights = weights[weights > threshold]
-    weights = -np.log(weights)  # TODO: which transformation to use?
-    print(len(weights))
+    if not baseline:
+        _, weights = predict_one_shower(shower, graph_embedder=graph_embedder, edge_classifier=edge_classifier)
+        weights = weights.detach().cpu().numpy()
+        edge_index = shower.edge_index.t().detach().cpu().numpy()
+        # weights = np.percentile(weights, q=90)
+        edge_index = edge_index[weights > threshold]
+        weights = weights[weights > threshold]
+        weights = -np.log(weights)  # TODO: which transformation to use?
+        print("Len weights", len(weights))
+    else:
+        weights = shower.edge_attr.view(-1).detach().cpu().numpy()
+        edge_index = shower.edge_index.t().detach().cpu().numpy()
+        # weights = np.exp(weights)
+        print(np.sort(weights))
+        edge_index = edge_index[weights < threshold]
+        weights = weights[weights < threshold]
+        print("Len weights", len(weights))
+
     for k, (p0, p1) in enumerate(edge_index):
         edges_to_add.append((p0, p1, weights[k]))
 
@@ -219,7 +273,7 @@ def calc_clustering_metrics(clusterized_bricks, experiment):
     ty_true = np.array(ty_true)
 
     r = HuberRegressor()
-    r.fit(X=E_raw.reshape((-1, 1)), y=E_true, sample_weight=1 / E_true)
+    r.fit(X=E_raw.reshape((-1, 1)), y=E_true, sample_weight=1 / E_true**6)
     E_pred = r.predict(E_raw.reshape((-1, 1)))
 
     scale_mm = 10000
@@ -256,49 +310,71 @@ def calc_clustering_metrics(clusterized_bricks, experiment):
     experiment.log_metric('MAE for ty', (np.abs((ty_raw - ty_true)).mean()))
 
 
+
 @click.command()
 @click.option('--datafile', type=str, default='./data/train_.pt')
 @click.option('--project_name', type=str, prompt='Enter project name', default='em_showers_clustering')
 @click.option('--work_space', type=str, prompt='Enter workspace name')
-@click.option('--dim_out', type=int, default=144)
 @click.option('--min_cl', type=int, default=40)
 @click.option('--cl_size', type=int, default=40)
 @click.option('--threshold', type=float, default=0.9)
+@click.option('--baseline', type=bool, default=False)
+@click.option('--hidden_dim', type=int, default=12)
+@click.option('--output_dim', type=int, default=12)
+@click.option('--num_layers', type=int, default=3)
 @click.option('--graph_embedder', type=str, default='GraphNN_KNN_v1')
 @click.option('--edge_classifier', type=str, default='EdgeClassifier_v1')
 @click.option('--graph_embedder_weights', type=str, default='GraphNN_KNN_v1')
 @click.option('--edge_classifier_weights', type=str, default='EdgeClassifier_v1')
 def main(
         datafile='./data/train_.pt',
-        dim_out=144,
-        min_cl=40,
+        hidden_dim=12,
+        output_dim=12,
+        num_layers=3,
         cl_size=40,
+        min_cl=40,
         threshold=0.9,
         project_name='em_showers_clustering',
         work_space='schattengenie',
         graph_embedder='GraphNN_KNN_v1',
         edge_classifier='EdgeClassifier_v1',
+        baseline=False,
+        graph_embedder_weights='GraphNN_KNN_v1', 
+        edge_classifier_weights='EdgeClassifier_v1'
 ):
-    experiment = Experiment(project_name=project_name, workspace=work_space)
+    experiment = Experiment(project_name=project_name, workspace=work_space, offline_directory="/home/vbelavin/comet_ml_offline")
     device = torch.device('cpu')
     showers = preprocess_dataset(datafile)
-
-    k = showers[0].x.shape[1]
-    print(k)
-    graph_embedder = str_to_class(graph_embedder)(dim_out=dim_out, k=k).to(device)
-    edge_classifier = str_to_class(edge_classifier)(dim_out=dim_out).to(device)
-
-    graph_embedder.load_state_dict(torch.load('graph_embedder.pt', map_location=device))
+    input_dim = showers[0].x.shape[1]
+    showers = DataLoader(showers, batch_size=1, shuffle=False)
+    edge_dim = 1
+    graph_embedder = str_to_class(graph_embedder)(
+        output_dim=output_dim,
+        hidden_dim=hidden_dim,
+        input_dim=input_dim,
+        num_layers=num_layers
+    ).to(device)
+    edge_classifier = str_to_class(edge_classifier)(
+        input_dim=2 * output_dim + edge_dim,
+    ).to(device)
+    if not baseline:
+        graph_embedder.load_state_dict(torch.load(graph_embedder_weights, map_location=device))
+        edge_classifier.load_state_dict(torch.load(edge_classifier_weights, map_location=device))
     graph_embedder.eval()
-    edge_classifier.load_state_dict(torch.load('edge_classifier.pt', map_location=device))
     edge_classifier.eval()
 
     clusterized_bricks = []
     for shower in showers:
-        G = preprocess_torch_shower_to_nx(shower,
-                                          graph_embedder=graph_embedder,
-                                          edge_classifier=edge_classifier,
-                                          threshold=threshold)
+        G = preprocess_torch_shower_to_nx(
+            shower,
+            graph_embedder=graph_embedder,
+            edge_classifier=edge_classifier,
+            threshold=threshold,
+            baseline=baseline
+        )
+        # TODO: cut succ and predecesive?
+        k_nearest_cut_succ(G, 25)
+        k_nearest_cut_pred(G, 25)
         graphx, clusters, roots = run_hdbscan_on_brick(G, min_cl=min_cl, cl_size=cl_size)
         clusters_graphx = []
         for cluster in clusters:

clustering_metrics.py
@@ -19,7 +19,7 @@ def class_disbalance_graphx__(graphx):
     return np.unique(signal, return_counts=True)
 
 
-def estimate_e(cluster, angle=0.05):
+def estimate_e(cluster, angle=0.1):
     x, y, z = estimate_start_xyz(cluster)
     tx, ty = estimate_txty(cluster)
     n = 0
@@ -27,16 +27,14 @@ def estimate_e(cluster, angle=0.05):
         dx = node['features']['SX'] - x
         dy = node['features']['SY'] - y
         dz = node['features']['SZ'] - z
-        dx = dx / dz - tx
-        dy = dy / dz - ty
-        dz = dz / dz
+        dx = dx / (dz + 1e-3) - tx
+        dy = dy / (dz + 1e-3) - ty
         if sqrt(dx ** 2 + dy ** 2) < angle:
             n += 1
-
     return n
 
 
-def estimate_start_xyz(cluster, k=3, shift_x=0., shift_y=0., shift_z=-2000.):
+def estimate_start_xyz(cluster, k=3, shift_x=0., shift_y=0., shift_z=0.):
     xs = []
     ys = []
     zs = []