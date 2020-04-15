5ade9ff4362ea2fb28dd9496579a2a0a4f16cc8d
vbelavin <belavin@phystech.edu>
2020-04-15 17:20:57 +0300

    min samples core added

--- run_clustering.sh
+++ run_clustering.sh
@@ -7,6 +7,7 @@ output_dim=${output_dim:-32}
 key=${key:-1}
 cl_size=${cl_size:-40}
 threshold=${threshold:-0.5}
+min_samples_core=${min_samples_core:4}
 while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
@@ -20,4 +21,4 @@ python clustering.py --datafile ./data/train_200_preprocessed.pt \
 --project_name em_showers_network_clustering --workspace SchattenGenie \
 --graph_embedder_weights graph_embedder_train_200_preprocessed_"$key".pt \
 --edge_classifier_weights edge_classifier_train_200_preprocessed_"$key".pt \
---cl_size $cl_size --min_cl $cl_size --threshold $threshold
+--cl_size $cl_size --min_cl $cl_size --threshold $threshold --min_samples_core $min_samples_core
